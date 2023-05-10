df2b6bc35c43db8230911ddffc43d71773b5a672
Son Jongchan <jcson.pro@gmail.com>
2023-05-10 09:08:16 +0900

    20230510

--- pystargram/users/views.py
+++ pystargram/users/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from users.forms import LoginForm
+
+
 def login_view(request):
-    return render(request, "users/login.html")
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
+
+    form = LoginForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "users/login.html", context)
