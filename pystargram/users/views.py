df57beda4cef5606bc08fc8b19d59b679c468528
Son Jongchan <jcson.pro@gmail.com>
2023-05-11 17:03:42 +0900

    219

--- pystargram/users/views.py
+++ pystargram/users/views.py
@@ -1,6 +1,43 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+from users.forms import LoginForm
+
+
 def login_view(request):
-    return render(request, "users/login.html")
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
+
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)      
+        # Form 클래스를 사용해 데이터를 받았다면, 반드시 is_valid()메서드를 호출해야 한다
+        print("form.is_valid():", form.is_valid())
+        # is_valid 메서드를 실행하기 전에는 cleaned_data에 접근할 수 없다
+        print("form.cleaned_data:", form.cleaned_data)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("/posts/feeds/")
+            else:
+                # print("로그인에 실패했습니다")
+                form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다")
+
+        context = {"form": form}
+        return render(request, "users/login.html", context)
+    else:
+        form = LoginForm()
+        context = {"form": form}
+        return render(request, "users/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+
+    return redirect("/users/login/")
