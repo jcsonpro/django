846cdebac08da6223a79aebd4293bd9c643a87d9
jcson.pro <jcson.pro@gmail.com>
2023-05-15 16:33:47 +0900

    248

--- pystargram/posts/admin.py
+++ pystargram/posts/admin.py
@@ -1,3 +1,46 @@
+import admin_thumbnails
 from django.contrib import admin
+from posts.models import PostImage, Post, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+
+    inlines = [
+        CommentInline,
+        PostImageInline,
+    ]
+
+
+@admin.register(PostImage)
+class PostImageAdimin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo"
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+    ]
