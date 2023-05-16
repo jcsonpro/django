c8ccbd48ae62ae7cfc809cca03ec61df3b66ce83
jcson.pro <jcson.pro@gmail.com>
2023-05-16 16:24:43 +0900

    291

--- pystargram/users/views.py
+++ pystargram/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 from users.forms import LoginForm, SignupForm
@@ -8,14 +9,14 @@ from users.models import User
 
 def login_view(request):
     if request.user.is_authenticated:
-        return redirect("/posts/feeds/")
+        return redirect("posts:feeds")
 
     if request.method == "POST":
         form = LoginForm(data=request.POST)
-        # Form 클래스를 사용해 데이터를 받았다면, 반드시 is_valid()메서드를 호출해야 한다
-        print("form.is_valid():", form.is_valid())
-        # is_valid 메서드를 실행하기 전에는 cleaned_data에 접근할 수 없다
-        print("form.cleaned_data:", form.cleaned_data)
+        # # Form 클래스를 사용해 데이터를 받았다면, 반드시 is_valid()메서드를 호출해야 한다
+        # print("form.is_valid():", form.is_valid())
+        # # is_valid 메서드를 실행하기 전에는 cleaned_data에 접근할 수 없다
+        # print("form.cleaned_data:", form.cleaned_data)
 
         if form.is_valid():
             username = form.cleaned_data["username"]
@@ -25,23 +26,25 @@ def login_view(request):
 
             if user:
                 login(request, user)
-                return redirect("/posts/feeds/")
+                return redirect("posts:feeds")
             else:
                 # print("로그인에 실패했습니다")
                 form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다")
 
         context = {"form": form}
-        return render(request, "users/login.html", context)
+        url = reverse("users:login")
+        return render(request, url, context)
     else:
         form = LoginForm()
         context = {"form": form}
-        return render(request, "users/login.html", context)
+        url = reverse("users:login")
+        print(url)
+        return render(request, url, context)
 
 
 def logout_view(request):
     logout(request)
-
-    return redirect("/users/login/")
+    return redirect("users:login")
 
 
 def signup(request):
@@ -50,10 +53,11 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("/posts/feeds/")
+            return redirect("posts:feeds")
 
     else:  # GET
         form = SignupForm()
 
     context = {"form": form}
-    return render(request, "users/signup.html", context)
+    url = reverse("users:signup")
+    return render(request, url, context)
